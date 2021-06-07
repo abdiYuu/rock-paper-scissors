92ad9d80458ba5acbe47c285f27a824080428710
Abdi Yusuf <abdiyuu7@gmail.com>
2021-06-07 12:31:07 -0600

    set game to 5 rounds. added scorekeeping logic

--- app.js
+++ app.js
@@ -8,42 +8,57 @@ function computerPlays() {
 }
 
 function rpsRound(e) {
-	computerChoice = computerPlays();
-	playerChoice = e.target.id;
 
+	if (rounds < 5) {
+		computerChoice = computerPlays();
+		playerChoice = e.target.id;
 
-	if (playerChoice === computerChoice) {
-		play_msg.innerText = `A tie!`;
-		return 'tie'
+		if (playerChoice === computerChoice) {
+			play_msg.innerText = `${playerChoice} ties ${computerChoice}`;
+			playerScore+=1;
+			computerScore+=1;
+	
 
-	}else if (playerChoice === 'ROCK' && computerChoice === 'PAPER' ||
-		  playerChoice === 'PAPER' && computerChoice === 'SCISSORS' ||
-		  playerChoice === 'SCISSORS' && computerChoice === 'ROCK') {
+		} else if (playerChoice === 'ROCK' && computerChoice === 'PAPER' ||
+			  playerChoice === 'PAPER' && computerChoice === 'SCISSORS' ||
+			  playerChoice === 'SCISSORS' && computerChoice === 'ROCK') {
 
-		play_msg.innerText = `PC Point. ${computerChoice} beats ${playerChoice}.`;
-		return 'loss'
+			play_msg.innerText = `PC Point. ${computerChoice} beats ${playerChoice}.`;
+			computerScore+=1;
 
-	}else if (playerChoice === 'PAPER' && computerChoice === 'ROCK' ||
-                  playerChoice === 'SCISSORS' && computerChoice === 'PAPER' ||
-                  playerChoice === 'ROCK' && computerChoice ==='SCISSORS') {
+		} else if (playerChoice === 'PAPER' && computerChoice === 'ROCK' ||
+	                  playerChoice === 'SCISSORS' && computerChoice === 'PAPER' ||
+	                  playerChoice === 'ROCK' && computerChoice ==='SCISSORS') {
 
-		play_msg.innerText = `Your Point! ${playerChoice} beats ${computerChoice}.`;
-		return 'win'
+			play_msg.innerText = `Your Point! ${playerChoice} beats ${computerChoice}.`;
+			playerScore+=1;
+		}
+		rounds++;
 	}
+
+	if (rounds == 5) {
+		choices.style.visibility='hidden'
+		play_msg.innerText = (playerScore > computerScore) ? 'Nice! You won!' :
+			(computerScore > playerScore) ? 'Aw. Better luck next time!' : 'A tie! Good game!';
+	}
+
 }
 
 function game(e) {
 	e.target.style.display = 'none'
-	const choices = document.querySelector('.choices')
 	choices.style.visibility='visible';
 
-	const buttons = document.querySelectorAll('.btn__rps');
-	for(let btn of buttons) {btn.addEventListener('click', rpsRound)}
-
 }
-
+const choices = document.querySelector('.choices')
 const play_msg = document.querySelector('.msg__top');
 const weapons = document.querySelector('.msg__bottom');
 
 const gameStart = document.querySelector('.btn__game');
 gameStart.addEventListener('click', game)
+
+let rounds = 0;
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll('.btn__rps');
+for(let btn of buttons) {btn.addEventListener('click', rpsRound)}
+
